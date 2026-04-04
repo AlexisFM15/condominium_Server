72633e39e313ba2a98811849b5bede4806041aad
AlexisFM15 <maximoalexisflorianmatos@gmail.com>
2026-04-04 00:46:50 -0400

    feat: validation for dates to schedule areas

--- src/services/schedule_area.service.ts
+++ src/services/schedule_area.service.ts
@@ -1,5 +1,15 @@
 import database from '../config/database.js'
 import { Schedule_area } from '../models/schedule_area.model.js'
 
-export const schedule_areaService =
-  database.appDataSource.getRepository(Schedule_area)
+export const schedule_areaService = database.appDataSource
+  .getRepository(Schedule_area)
+  .extend({
+    findByDateAndArea(area: number, reservation_date: Date) {
+      return this.createQueryBuilder('sa')
+        .where('sa.reservation_date = :reservation_date', {
+          reservation_date: reservation_date,
+        })
+        .andWhere('sa.areaId = :area', { area: area })
+        .getOne()
+    },
+  })
